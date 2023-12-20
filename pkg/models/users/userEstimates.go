da9acf7f1bf15b4b6aaaa9c8e28b64f5f64c1f37
Smuggs <102694816+Smuggler911@users.noreply.github.com>
2023-12-20 14:13:44 +0400

    feat: uuid generation

--- pkg/models/users/userEstimates.go
+++ pkg/models/users/userEstimates.go
@@ -6,6 +6,5 @@ type UserEstimates struct {
 	Id     uuid.UUID `gorm:"PrimaryKey" json:"id"`
 	Name   string    `json:"name"`
 	Grade  string    `json:"grade"`
-	UserId uuid.UUID `json:"user_id"`
-	User   User      `gorm:"foreignKey:UserId"`
+	UserId uuid.UUID `gorm:"foreignKey:Id" json:"user_id"`
 }
